fc8be5109bd09669467adae2c4d5d3648ecf1f83
Han <xsytby1112@gmail.com>
2018-09-07 17:34:23 +0800

    add some webpack config

--- app/config-overrides.js
+++ app/config-overrides.js
@@ -1,9 +1,24 @@
-const { injectBabelPlugin } = requrie("react-app-rewired");
+const customConfig = require("./config");
+const rewireLess = require("react-app-rewire-less");
+const {injectBabelPlugin} = require("react-app-rewired");
 
 module.exports = function override(config, env) {
     config = injectBabelPlugin(
-        ["import", { libraryName: "antd", libraryDirectory: "es", style: "css" }],
+        ["import", {libraryName: "antd", libraryDriectory: "es", style: "true"}],
         config,
     );
+
+    config = customConfig.rewireCleanWebpackPlugin({
+        pathsToClean: ["build"],
+        cleanOptions: {
+            root: __dirname
+        }
+    })(config, env)
+
+    config = rewireLess.withLoaderOptions({
+        modifyVars: { "@primary-color": "#1DA57A" },
+        javascriptEnabled: true
+    })(config, env)
+
     return config
 }
---
+++ app/config/index.js
@@ -0,0 +1,5 @@
+const rewireCleanWebpackPlugin = require("./rewire-clean-webpack-plugin.js");
+
+module.exports = {
+    rewireCleanWebpackPlugin,
+}
